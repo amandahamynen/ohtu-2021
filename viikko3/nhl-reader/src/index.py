bbebd1202ba43f2341ef7969d344f7601f91cfb1
Amanda <amanda.hamynen@gmail.com>
2021-11-15 17:07:52 +0200

    Tehtävät 1 ja 2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,31 @@
+import requests
 from player import Player
+from datetime import datetime
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'], player_dict['team'], player_dict['goals'], player_dict['assists'], player_dict['nationality']
+        )
+
+        players.append(player)
+        
+    players.sort(key = lambda x: x.get_score(), reverse=True)
+
+    print(f"Players from FIN {datetime.now()}")
+    print()
+
+    for player in players:
+        if player.nationality == 'FIN':
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.nationality = nationality
+
+    def get_score(self):
+        return self.goals + self.assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team} {str(self.goals):>2} + {str(self.assists):>2} = {self.goals + self.assists}"
